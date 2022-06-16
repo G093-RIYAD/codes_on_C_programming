aaa1a137ec106ef9281c7ba1a888be3017293485
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-16 22:28:57 +0600

    Update 2nd_day_on.c

--- 2nd_day_on.c
+++ 2nd_day_on.c
@@ -3,5 +3,7 @@
 int main(){
     printf("My Salary is %d in Bnagladeshi TK.\n",1000000);
     printf("%d + %d = %d",10,5,15);
+    printf("The hash character is: %c\n",'#');
+    printf("My Height: %f feet and weight: %d kg.\n",5.10,60);
     return 0 ;
 }
