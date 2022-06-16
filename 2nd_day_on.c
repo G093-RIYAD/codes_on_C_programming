e256349bc2e4d75a5273a20f250713a77f063d7a
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-16 22:29:33 +0600

    Update 2nd_day_on.c

--- 2nd_day_on.c
+++ 2nd_day_on.c
@@ -5,5 +5,11 @@ int main(){
     printf("%d + %d = %d",10,5,15);
     printf("The hash character is: %c\n",'#');
     printf("My Height: %f feet and weight: %d kg.\n",5.10,60);
+    
+    int x = 10;
+    int y = 20;
+    printf("%d+%d=%d\n",x,y,x+y);
+    x=x*y-20.;
+    printf("Final Value of x ix: %d\n",x);
     return 0 ;
 }
