6442424eb67a5497bae0754bc89a6f1c5c2f95c2
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-18 19:53:50 +0600

    Update 3rd_day.c

--- 3rd_day.c
+++ 3rd_day.c
@@ -1,21 +1,34 @@
-/* C Program to Count Total Notes in a Given Amount */
-#include<stdio.h>
- 
-int main()
-{
- int a[8] = {500, 100, 50, 20, 10, 5, 2, 1};
- int Amount, i, temp;
- 
- printf("\n Please Enter the Amount of Cash  =  ");
- scanf("%d", &Amount);
- 
- temp = Amount;
-  
- for(i = 0; i < 8; i++)
- {
- 	printf("\n %d Notes is = %d", a[i], temp / a[i]);
- 	temp = temp % a[i];
- }
+/***************************************************************
+ Program to find the number of denominations for a given amount
+ ***************************************************************/
 
- return 0;
+#include <stdio.h>
+#define SIZE 6
+
+int main() 
+{    
+    int amount, notes;   
+
+    // currency denominations
+    int denominations[SIZE] = {500, 100, 50, 20, 10, 1};
+
+    printf("Enter amount: ");
+    scanf("%d", &amount);
+
+    printf("\n");
+
+    for (int i = 0; i < SIZE; i++) 
+    {
+        notes = amount / denominations[i];
+
+        if (notes)
+        {
+            amount = amount % denominations[i]; // remaining money
+
+            printf("%d * %d = %d \n", notes, denominations[i], 
+                notes * denominations[i]);
+        }                
+    }
+
+    return 0;
 }
