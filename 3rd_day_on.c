662debf060a78c7cf4249b2ee84d53fad617e16e
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-19 20:21:58 +0600

    Update 3rd_day_on.c

--- 3rd_day_on.c
+++ 3rd_day_on.c
@@ -5,5 +5,8 @@ int main()
     MY_char = 'k';
     MY_char = '3';
     printf("%c\n",MY_char);
+    
+    char  a = 'm', b = 'u', c = 's', d =  'a';
+    printf("%c%c%c%c\n",a,b,c,d);
     return 0;
 }
