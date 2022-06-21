5b6db1550e612b11afa931e867bfcfb69dceeb19
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-21 21:42:15 +0600

    Create 5th_day_on.c

---
+++ 5th_day_on.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    //01-Write a C Program to Calculate Area of a Square, take length of one side as user input.
+    int a;
+    printf("If one side of length : ");
+    scanf("%d",&a);
+    printf("\nThen the area of Square is: %d",a * a);
+    return 0;
+}
