b877d98dfd7c6dc7a0c02baf8af674797fd6cf8f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-21 21:54:11 +0600

    Update 5th_day_on.c

--- 5th_day_on.c
+++ 5th_day_on.c
@@ -6,5 +6,12 @@ int main()
     printf("If one side of length : ");
     scanf("%d",&a);
     printf("Then the area of Square is: %d",a * a);
+    
+    //Write a C program to enter temperature in °Celsius and convert it into °Fahrenheit.
+    float celsius = 16, fahrenheit;
+    fahrenheit = (celsius *9/5) +32;
+    printf("%.2f celsius : %.2f fahrenheit",celsius, fahrenheit);
+    
+    
     return 0;
 }
