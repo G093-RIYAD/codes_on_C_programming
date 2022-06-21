403719813185d017243ccc56f40d227cc49ef10b
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-21 22:41:12 +0600

    Update 5th_day_on.c

--- 5th_day_on.c
+++ 5th_day_on.c
@@ -17,5 +17,13 @@ int main()
     fahr = 70;
     cel = ((fahr-32)*5/9);
     printf("%.2f fahr : %.2f cel",fahr,cel);
+    
+    //04-Write a C program to enter marks of five subjects and calculate total and average marks.
+    int v=70,w=76,x=74,y=85,z=80,total,average;
+    printf("All the subject's and there marks: \nv: %d\nw: %d\nx: %d\ny: %d\nz: %d\n",v,w,x,y,z);
+    total = v+w+x+y+z;
+    printf("Total marks: %d\n",total);
+    average = total/5;
+    printf("The average marks: %d",average);
     return 0;
 }
