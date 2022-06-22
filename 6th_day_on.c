f09dee814120f93855170c264d95b3b4aa57eca0
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-22 22:26:07 +0600

    Update 6th_day_on.c

--- 6th_day_on.c
+++ 6th_day_on.c
@@ -25,5 +25,17 @@ int main() {
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %zu bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
+    
+    int a;
+    long b;   // equivalent to long int b;
+    long long c;  // equivalent to long long int c;
+    double e;
+    long double f;
+
+    printf("Size of int = %zu bytes \n", sizeof(a));
+    printf("Size of long int = %zu bytes\n", sizeof(b));
+    printf("Size of long long int = %zu bytes\n", sizeof(c));
+    printf("Size of double = %zu bytes\n", sizeof(e));
+    printf("Size of long double = %zu bytes\n", sizeof(f));
     return 0;
 }
