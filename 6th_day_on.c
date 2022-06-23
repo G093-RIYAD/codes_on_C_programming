cf0d3974c969ac66bd940456af90df008b68fb9f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-23 21:18:18 +0600

    Update 6th_day_on.c

--- 6th_day_on.c
+++ 6th_day_on.c
@@ -1,43 +1,5 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
-
-    // Computes quotient
-    quotient = dividend / divisor;
-
-    // Computes remainder
-    remainder = dividend % divisor;
-
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
-
-    // sizeof evaluates the size of a variable
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
-    int a;
-    long b;
-    // equivalent to long int b;        
-    long long c;  
-    // equivalent to long long int c;
-    double e;
-    long double f;
-
-    printf("Size of int = %zu bytes \n", sizeof(a));
-    printf("Size of long int = %zu bytes\n", sizeof(b));
-    printf("Size of long long int = %zu bytes\n", sizeof(c));
-    printf("Size of double = %zu bytes\n", sizeof(e));
-    printf("Size of long double = %zu bytes\n", sizeof(f));
+   
     return 0;
 }
