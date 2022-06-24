bb38914adbe5a43c5bb9a64bcad94b5d7e5afbe9
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-24 22:06:16 +0600

    Update 7th_day_on.c

--- 7th_day_on.c
+++ 7th_day_on.c
@@ -10,5 +10,18 @@ int main()
     else{
         printf("The number is not divisible by 3 or 5.");
     }
+    
+    /*Write a program which will take a positive integer as input
+    from the user and print whether the integer is EVEN or ODD.*/
+    int a,b;
+    printf("Input a integer value: ");
+    scanf("%d",&a);
+    b = a % 2;
+    if(b == 0){
+        printf("%d is an EVEN number.",a);
+    }
+    else{
+        printf("%d is an ODD number.",a);
+    }
     return 0;
 }
