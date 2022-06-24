e2d480aacac810ec8fc9b7a8034a4536a5c134b4
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-24 21:18:16 +0600

    Update 7th_day_on.c

--- 7th_day_on.c
+++ 7th_day_on.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the Value of a: ");
+    scanf("%d",&a);
+    if(a % 3 == 0 || a % 5 == 0){
+        printf("The number is divisible by 3 or 5.");
+    }
+    else{
+        printf("The number is not divisible by 3 or 5.");
+    }
+    return 0;
+}
