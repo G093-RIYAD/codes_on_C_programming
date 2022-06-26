c2665a5f7bfb15eb131a4533d69fc98f8130a4bd
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-26 21:40:00 +0600

    Update 7th_day_on.c

--- 7th_day_on.c
+++ 7th_day_on.c
@@ -127,4 +127,14 @@ int main()
     }
     return 0;
 }
-
+#include<stdio.h>
+int main()
+{
+	int num1=1;
+	int num2=2;
+	if(num1<num2)		//test-condition
+	{
+		printf("num1 is smaller than num2");
+	}
+	return 0;
+}
