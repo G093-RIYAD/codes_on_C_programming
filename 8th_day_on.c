f2c93ca21201b4a8ea7cc71a6b22439026d18ae1
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-28 22:17:16 +0600

    Update 8th_day_on.c

--- 8th_day_on.c
+++ 8th_day_on.c
@@ -110,3 +110,18 @@ int main() {
         }
         return 0;
     }
+#include <stdio.h>
+int main() {
+int language = 10;
+  switch (language) {
+  case 1:
+    printf("C#\n");
+    break;
+  case 2:
+    printf("C\n");
+    break;
+  case 3:
+    printf("C++\n");
+    break;
+  default:
+    printf("Other programming language\n");}}
