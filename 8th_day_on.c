bb1ae40d6edb750dfce3ff1e428b4f2e4a3eeb31
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-29 20:44:41 +0600

    Update 8th_day_on.c

--- 8th_day_on.c
+++ 8th_day_on.c
@@ -1,1 +1,31 @@
-
+#include <stdio.h>
+int main()
+{
+    int day ;
+    printf("Type a number of of week: ");
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+            printf("Saturday");
+            break;
+        case 2:
+            printf("Sunday");
+            break;
+        case 3:
+            printf("Monday");
+            break;
+        case 4:
+            printf("Tuesday");
+            break;
+        case 5:
+            printf("Wednesday");
+            break;
+        case 6:
+            printf("Thursday");
+            break;
+        default:
+            printf("Friday");
+            break;
+    }
+    return 0;
+}
