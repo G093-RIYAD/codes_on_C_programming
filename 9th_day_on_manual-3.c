7237f7d4d7621a95ee37a6b0de19cece12885b46
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-30 21:58:19 +0600

    Update 9th_day_on_manual-3.c

--- 9th_day_on_manual-3.c
+++ 9th_day_on_manual-3.c
@@ -28,3 +28,28 @@ int main()
     }
     return 0;
 }
+// #03 Write a Program to take the value from the user as input any alphabet and check whether it is vowel or consonant (Using the switch statement).
+#include <stdio.h>
+int main()
+{
+    char v;
+    printf("Type any sengle alphabet: ");
+    scanf("%c",&v);
+    switch(v){
+        case'A':
+        case'E':
+        case'I':
+        case'O':
+        case'U':
+        case'a':
+        case'e':
+        case'i':
+        case'o':
+        case'u':
+            printf("Vowel");
+            break;
+        default:
+            printf("Consonant");
+    }
+    return 0;
+}
