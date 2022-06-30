3d4154286bc5dc9d521df2cb630cb9a963d12b65
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-30 23:13:17 +0600

    Update 9th_day_on_manual-3.c

--- 9th_day_on_manual-3.c
+++ 9th_day_on_manual-3.c
@@ -53,3 +53,18 @@ int main()
     }
     return 0;
 }
+// #04 Write a C program to check whether a year is leap year or not.
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Type a year: ");
+    scanf("%d",&year);
+    if(year % 400 == 0 && year % 100 != 0 || year % 4 == 0){
+        printf("%d is a leap year",year);
+    }
+    else{
+        printf("%d is not a  leap year",year);
+    }
+    return 0;
+}
