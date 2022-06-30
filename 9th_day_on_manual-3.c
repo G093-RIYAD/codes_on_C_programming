9bb180220743a29a316a12fa32e4c0f46b80769f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-30 21:34:11 +0600

    Update 9th_day_on_manual-3.c

--- 9th_day_on_manual-3.c
+++ 9th_day_on_manual-3.c
@@ -12,4 +12,19 @@ int main()
     }
     return 0;
 }
-
+// #02 Write a C program to find maximum between three numbers.
+#include <stdio.h>
+int main()
+{
+    int a=49, b=59,c=39;
+    if(a>b && a>c){
+        printf("a has the maximum number between three variable");
+    }
+    else if(b>a && b>c){
+        printf("b has the maximum number between three variable");
+    }
+    else{
+        printf("c has the maximum number between three variable");
+    }
+    return 0;
+}
