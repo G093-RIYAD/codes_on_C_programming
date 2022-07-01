0f2f74fec4fcb8617134427343be4ea26dcf5ffa
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-01 22:53:27 +0600

    Update 10th_day_on.c

--- 10th_day_on.c
+++ 10th_day_on.c
@@ -77,3 +77,10 @@ printf("Enter your  name plz: ");
 fgets(name, 10, stdin);
 printf("My name is %s ",name);
 return 0;}
+#include <stdio.h>
+int main()
+{char town[40];
+  printf("Enter your town: ");
+  gets(town);
+  fputs(town, stdout);
+  return 0;}
