7e592df311769a8f3715550bb371fe165af053c1
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-05 22:53:36 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -105,3 +105,26 @@ void main()
     printf ("%d is equivalent to %d years, %d weeks and %d daysn",
             ndays, year, week, days);
 }
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    int number;
+    printf("Please enter a number:\n");
+    scanf("%d",&number);
+    /*
+        For single statements we can skip the curly brackets
+    */
+    if(number < 100)
+        printf("Number is less than 100!\n");
+    else if(number == 100)
+        printf("Number is 100!\n");
+    else
+        printf("Number is greater than 100!\n");
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    
+    return 0;
+}
