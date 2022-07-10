c9ce28b9915785fc13407343df3623bb169f994e
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-10 23:00:13 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -28,7 +28,7 @@ int main () {
 /* function returning the max between two numbers */
 int max(int num1, int num2) {
 
-   /* local variable declaration */
+   
    int result;
  
    if (num1 > num2)
