ec185f93b45b2794f1676c54fec6ea3cf2522af1
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-11 20:11:49 +0600

    Update 13th_day_on.c

--- 13th_day_on.c
+++ 13th_day_on.c
@@ -8,3 +8,14 @@ int main()
     }
     return 0;
 }
+// EVEN numbers
+#include<stdio.h>
+int main()
+{
+    int a=2;
+    while(a<=100){
+        printf("%d\n",a);
+        a=a+2;
+    }
+    return 0;
+}
