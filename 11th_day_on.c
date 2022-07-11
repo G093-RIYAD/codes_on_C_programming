0d982d5ef64e3d2cdf2a51f2b8b0fd3fbe78c943
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-11 20:59:38 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -50,10 +50,7 @@ int main(){
    return 0;
 }
 
- 
-    printf("Enter the number of days\n");
-    scanf("%d", &ndays);
-    year = ndays / 365;
+
     week =(ndays % 365) / DAYSINWEEK;
     days =(ndays % 365) % DAYSINWEEK;
     printf ("%d is equivalent to %d years, %d weeks and %d daysn",
