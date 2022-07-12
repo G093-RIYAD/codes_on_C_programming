4bff307aba246a1126e799bfc4795cb2db007644
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-12 22:03:54 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -66,3 +66,24 @@ num = num / 10;
 printf("\nREVERSED NUMBER IS----: %ld", rev);
 return 0;
 }
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+long int n, num, rev = 0, dig;
+
+printf("\n\nENTER A NUMBER----: ");
+scanf("%ld", &num);
+n = num;
+while(num>0)
+{
+dig = num % 10;
+rev = rev * 10 + dig;
+num = num / 10;
+}
+if (n == rev)
+printf("\nGIVEN NUMBER IS A PALINDROME");
+else
+printf("\nGIVEN NUMBER IS NOT A PALINDROME");
+return 0;
+}
