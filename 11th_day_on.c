664889542cda568d18dcc7dca04c32d27423fba6
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-12 22:03:30 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -49,3 +49,20 @@ int main(){
       printf("The given Triangle is scalene\n");
    return 0;
 }
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+long int num, rev = 0, dig;
+
+printf("\n\nENTER A NUMBER-----: ");
+scanf("%ld", &num);
+while(num>0)
+{
+dig = num % 10;
+rev = rev * 10 + dig;
+num = num / 10;
+}
+printf("\nREVERSED NUMBER IS----: %ld", rev);
+return 0;
+}
