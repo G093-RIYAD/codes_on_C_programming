e8d8975df47e10bad6be11116653460e6321912f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-14 21:46:28 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -49,3 +49,27 @@ int main(){
       printf("The given Triangle is scalene\n");
    return 0;
 }
+/* program to reverse an integer number.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int dig, revNumber;
+
+    printf("Enter an integer number : ");
+    scanf("%d", &n);
+
+    /*Reversing Number*/
+    revNumber = 0;
+
+    while (n > 0) {
+        dig = n % 10; /*get digit*/
+        revNumber = (revNumber * 10) + dig;
+        n = n / 10;
+    }
+
+    printf("Reverse Number is : %d\n", revNumber);
+    return 0;
+}
