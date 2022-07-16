02cdd75a447c9050619a1f83270eef4bfa57744b
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-16 21:29:41 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -44,5 +44,3 @@ int main(){
    printf("Enter sides of triangle: \n");
    scanf("%d%d%d",&side1,&side2,&side3);
    if(side1 == side2 && side2 == side3)
-      printf("The Given Triangle is equilateral\n");
-   else
