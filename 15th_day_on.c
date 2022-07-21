0e256e9f490b3d9aae897fbef24d4a666acb1ed2
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-21 15:52:27 +0600

    Create 15th_day_on.c

---
+++ 15th_day_on.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+     int i, j, f;
+     for(i = 1; i<=3; i++){
+        for(j=1; j<=2; j++){
+            for(f=1; f<=5; f++){
+                printf("Hk");
+                printf("\n");
+            }
+        }
+     }
+     return 0;
+ }
