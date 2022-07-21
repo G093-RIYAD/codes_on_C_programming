680e0417fa094543b06cb995ba272f396dfc676e
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-21 16:06:00 +0600

    Update 15th_day_on.c

--- 15th_day_on.c
+++ 15th_day_on.c
@@ -12,3 +12,21 @@
      }
      return 0;
  }
+#include<stdio.h>
+ int main()
+ {
+     int i, j, f, l;
+     for(i = 1; i<=5; i++){
+        for(j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+     }
+     for(f = 1; f<=4; f++){
+        for(l=f; l<=4; l++){
+            printf("*");
+        }
+        printf("\n");
+     }
+     return 0;
+ }
