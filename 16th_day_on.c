a21ad870fdf4d451dbe09ca5b48889be9fd2832b
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-22 22:46:05 +0600

    Update 16th_day_on.c

--- 16th_day_on.c
+++ 16th_day_on.c
@@ -65,3 +65,16 @@ int main()
     }
     return 0;
 }
+#include<stdio.h>
+int main()
+{
+    float a[5];
+    int i;
+    for(i=0; i<5; i++){
+        scanf("%f",&a[i]);
+    }
+    for(i=0; i<5; i++){
+        printf("%.2f\n",a[i]);
+    }
+    return 0;
+}
