c46d1f227984b1d5de9d696e09f0dff960bfe2d7
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-22 20:08:43 +0600

    Update 16th_day_on.c

--- 16th_day_on.c
+++ 16th_day_on.c
@@ -1,8 +1,13 @@
-int i;
-
-for (i = 0; i < 10; i++) {
-  if (i == 4) {
-    break;
-  }
-  printf("%d\n", i);
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i = 0; i < 10; i++) {
+        if (i == 4) {
+        break;
+        }
+    printf("%d\n", i);
+    }
+    return 0;
 }
+
