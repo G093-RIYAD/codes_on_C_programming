6585216d4f84b073c963f4f82b5c9777ee89a37f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-22 20:32:46 +0600

    Update 16th_day_on.c

--- 16th_day_on.c
+++ 16th_day_on.c
@@ -51,3 +51,17 @@ int main()
     }
     return 0;
 }
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    while(i < 10) {
+        if(i%2==0){
+            i++;
+            continue;
+        }
+    printf("%d\n", i);
+    i++;
+    }
+    return 0;
+}
