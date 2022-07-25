86b877453ecfed6f40a2bb1295ec6135f75a4b5f
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-25 21:15:32 +0600

    Add files via upload

---
+++ 12th_day.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num,a,b;
+    printf("Enter your desair number of star: ");
+    scanf("%d",&num);
+    for(a=1;a<=num;a++){
+        for(b=1;b<=a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(a=num-1;a>=1;a--){
+        for(b=1;b<=a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
