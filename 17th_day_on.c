582b026a150987c37f1195210e24f2efbbe67368
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-25 21:18:00 +0600

    Update and rename 12th_day.c to 17th_day_on.c

--- 17th_day_on.c
+++ 17th_day_on.c
@@ -18,3 +18,23 @@ int main()
     }
     return 0;
 }
+#include <stdio.h>
+int main()
+{
+    int num,a,b;
+    printf("Enter your desair number of star: ");
+    scanf("%d",&num);
+    for(a=1;a<=num;a++){
+        for(b=1;b<=a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(a=num-1;a>=1;a--){
+        for(b=1;b<=a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
