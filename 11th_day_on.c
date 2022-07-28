2d5ef93a166b3795c47261964fd099128b8ba402
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-07-28 20:52:19 +0600

    Update 11th_day_on.c

--- 11th_day_on.c
+++ 11th_day_on.c
@@ -38,3 +38,23 @@ int max(int num1, int num2) {
  
    return 0; 
 }	
+#include <stdio.h>
+
+int main() {
+  int values[5];
+
+  printf("Enter 5 integers: ");
+
+  // taking input and storing it in an array
+  for(int i = 0; i < 5; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying integers: ");
+
+  // printing elements of an array
+  for(int i = 0; i < 5; ++i) {
+     printf("%d\n", values[i]);
+  }
+  return 0;
+}
